ad255ceb98a9a11a29306e3616fd204d8af0ec7a
Juan David <juandavid918@gmail.com>
2023-11-19 10:04:16 -0500

    Puerto 8080

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("<h1>Express con HTML!!</h1>"));
 
-app.listen(8000);
+app.listen(8080);
 
-console.log("Server on port 8000");
+console.log("Server on port 8080");
