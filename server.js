276a04c0e8cabb51d6165173c7bedbc6b014fa1c
Juan David <juandavid918@gmail.com>
2023-11-19 10:00:08 -0500

    Texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world with Express!"));
+app.get("/", (req, res) => res.send("<h1>Express con HTML!!</h1>"));
 
 app.listen(3000);
 
